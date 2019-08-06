66c59e897d12bb7e9afe1c2a24822628bb70cde0
leigh-perry <lperry.breakpoint@gmail.com>
2019-08-06 11:46:03 +1000

    master: Remove jdk stuff from haskell shell

--- dev-haskell/dev-haskell.nix
+++ dev-haskell/dev-haskell.nix
@@ -1,25 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 
-let
-  cfg = (import ../jdk/jdk11.nix);
-  jdk-name = cfg.jdk-name;
-  jdk-sha = cfg.jdk-sha;
-in
-
 stdenv.mkDerivation rec {
   name = "dev-haskell";
-
-  local-jdk11 =
-    callPackage ../jdk/shared-jdk.nix {
-      inherit jdk-name;
-      inherit jdk-sha;
-    };
-
-  sbt-jdk11 =
-    sbt.override {
-      jre = local-jdk11;
-    };
 
   buildInputs = [
     git
@@ -28,9 +11,9 @@ stdenv.mkDerivation rec {
     ghc
     cabal-install
     stack
-    haskellPackages.ghcid
     hlint
     haskellPackages.hindent
+    haskellPackages.ghcid
     cabal2nix
     jq
     shellcheck
