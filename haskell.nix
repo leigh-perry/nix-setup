7a7d3cdcd98d49844519abf29873c9c6f70eada3
leigh-perry <lperry.breakpoint@gmail.com>
2020-01-02 23:38:49 +1100

    Update nix package version

--- haskell.nix
+++ haskell.nix
@@ -3,9 +3,9 @@ with import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
   name = "nixos-unstable-2019-11-25";
   # Commit hash for nixos-unstable as of 2019-11-25 - get from head (git log)
-  url = https://github.com/nixos/nixpkgs/archive/aa7efea848f5f936a86ea3c4dcd582df0b57699d.tar.gz;
+  url = https://github.com/nixos/nixpkgs/archive/091ddf2650952a5eda3d8e1b8adbf960c8b492b4.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "16wzghfxl9a4lvmvhxy6vgkdvb3b77dr9avbzzgfgxp2bcafbavb";
+  sha256 = "1mkq6y5wb9pbafvhfj720s29p8smdcrssz9qvvwshfzbjhaj4jal";
 }) {};
 
 let
@@ -39,7 +39,7 @@ in
       input-cabal2nix
 
       pointfree
-      # pointful
+      #pointful
     ];
   
     shellHook = ''
