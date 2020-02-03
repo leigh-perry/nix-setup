70f33b9a1327af831b80a60a17af98f035c21743
leigh-perry <lperry.breakpoint@gmail.com>
2020-02-03 22:11:45 +1100

    Update pinned version

--- aws.nix
+++ aws.nix
@@ -1,11 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 with import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "nixos-unstable-2019-11-25";
-  # Commit hash for nixos-unstable as of 2019-11-25 - get from head (git log)
-  url = https://github.com/nixos/nixpkgs/archive/091ddf2650952a5eda3d8e1b8adbf960c8b492b4.tar.gz;
+  name = "nixos-unstable-2020-02-02";
+  # Commit hash for nixos-unstable as of 2020-02-02 - get from head (git log)
+  url = https://github.com/nixos/nixpkgs/archive/7870644286e1ba555cbe65932fa8ac7f596b6efa.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1mkq6y5wb9pbafvhfj720s29p8smdcrssz9qvvwshfzbjhaj4jal";
+  sha256 = "0jsa055gs1kgxj2xf4bf462l0p4g87p7d9i68718b654h1chy2pb";
 }) {};
 
 let
