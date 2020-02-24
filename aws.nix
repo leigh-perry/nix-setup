98fea7dfd495c9bdd59350dfb397c2d87e6c0c99
leigh-perry <lperry.breakpoint@gmail.com>
2020-02-25 08:56:09 +1100

    Pin to latest versions except for Terraform which is pinned to 0.12.20

--- aws.nix
+++ aws.nix
@@ -3,9 +3,9 @@ with import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
   name = "nixos-unstable-2020-02-02";
   # Commit hash for nixos-unstable as of 2020-02-02 - get from head (git log)
-  url = https://github.com/nixos/nixpkgs/archive/7870644286e1ba555cbe65932fa8ac7f596b6efa.tar.gz;
+  url = https://github.com/nixos/nixpkgs/archive/4928d8ca89261d4fbd1811324e402a6025c77574.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "0jsa055gs1kgxj2xf4bf462l0p4g87p7d9i68718b654h1chy2pb";
+  sha256 = "12r432bp0lszczrdcliqfa32qa70fbv0fdq74fw55q0mibn9sg68";
 }) {};
 
 let
