ed01fb5fb5083685d43abad1677e2581b8f7032d
Adam Evans <260708+forficate@users.noreply.github.com>
2020-03-17 22:19:32 +1100

    Fixes https://github.com/leigh-perry/nix-setup/issues/5
    Set the JDK/JRE globally to 11.

--- scala.nix
+++ scala.nix
@@ -6,41 +6,38 @@ with import (builtins.fetchTarball {
   url = https://github.com/nixos/nixpkgs/archive/4928d8ca89261d4fbd1811324e402a6025c77574.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
   sha256 = "12r432bp0lszczrdcliqfa32qa70fbv0fdq74fw55q0mibn9sg68";
-}) {};
+}) {
+  config = {
+    packageOverrides = pkgs: {
+      jdk = pkgs.jdk11;
+      jre = pkgs.jdk11;
+    };
+  };
+};
 
-let
-  use-jdk = jdk11;   # callPackage jdk/shared-jdk.nix { inherit jdk-name; inherit jdk-sha; };
-  pkgs = import <nixpkgs> { overlays = [ (self: super: {
-    jdk = use-jdk;
-    jre = use-jdk;
-  }) ]; }; 
-  # cfg = (import jdk/jdk11.nix);
-  # jdk-name = cfg.jdk-name;
-  # jdk-sha = cfg.jdk-sha;
-in
-  stdenv.mkDerivation rec {
-    name = "Scala";
+stdenv.mkDerivation rec {
+  name = "Scala";
 
-    buildInputs = [
-      #figlet
-      use-jdk
-      sbt
-      bloop
-      ammonite
-      gradle
-      maven
+  buildInputs = [
+    jdk
+    sbt
+    bloop
+    ammonite
+    gradle
+    maven
 
-      # For ci-release / travis / mavencentral / sonatype
-      gnupg
-      
-      # For scalajs
-      nodejs
+    # For ci-release / travis / mavencentral / sonatype
+    gnupg
 
-      # For docusaurus
-      yarn
-    ];
+    # For scalajs
+    nodejs
+
+    # For docusaurus
+    yarn
+  ];
 
     shellHook = ''
       #figlet -w 160 "${name}"
+      export JAVA_HOME="${jdk.home}"
     '';
   }
