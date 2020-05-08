3b8411b285db5cfa3cd1f47de25edebf80ab89a1
leigh-perry <lperry.breakpoint@gmail.com>
2020-05-08 18:24:31 +1000

    Update versions

--- dotty.nix
+++ dotty.nix
@@ -1,12 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 with import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "nixos-unstable-2020-02-02";
-  # Commit hash for nixos-unstable as of 2020-02-02 - get from head (git log)
-  url = https://github.com/nixos/nixpkgs/archive/4928d8ca89261d4fbd1811324e402a6025c77574.tar.gz;
+  name = "nixos-unstable-2020-05-08";
+  # Commit hash for nixos-unstable as of 2020-05-08 - get from head (git log)
+  url = https://github.com/nixos/nixpkgs/archive/d78ba41a5604c8e06d40756a2436e52169354d36.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "12r432bp0lszczrdcliqfa32qa70fbv0fdq74fw55q0mibn9sg68";
-}) {};
+  sha256 = "0bk81ddx1iq8i0nhg1i44kllihphyzhbc416zgylli0ycphknrkv";}) {};
 
 let
   use-jdk = jdk11;   # callPackage jdk/shared-jdk.nix { inherit jdk-name; inherit jdk-sha; };
