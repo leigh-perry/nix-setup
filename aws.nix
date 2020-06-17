9f5fc4e7bbbe9bf618e3f4d688fadffee45294e2
leigh-perry <lperry.breakpoint@gmail.com>
2020-06-17 11:00:26 +1000

    Code formatting

--- aws.nix
+++ aws.nix
@@ -1,11 +1,16 @@
 let
-  pkgs = import (builtins.fetchTarball {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-unstable-2020-05-08";
-    # Commit hash for nixos-unstable as of 2020-05-08 - get from head (git log)
-    url = https://github.com/nixos/nixpkgs/archive/d78ba41a5604c8e06d40756a2436e52169354d36.tar.gz;
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0bk81ddx1iq8i0nhg1i44kllihphyzhbc416zgylli0ycphknrkv";}) {};
+  pkgs =
+    import (
+      builtins.fetchTarball {
+        # Descriptive name to make the store path easier to identify
+        name = "nixos-unstable-2020-05-08";
+        # Commit hash for nixos-unstable as of 2020-05-08 - get from head (git log)
+        url = https://github.com/nixos/nixpkgs/archive/d78ba41a5604c8e06d40756a2436e52169354d36.tar.gz;
+        # Hash obtained using `nix-prefetch-url --unpack <url>`
+        sha256 = "0bk81ddx1iq8i0nhg1i44kllihphyzhbc416zgylli0ycphknrkv";
+      }
+    ) {
+    };
 
   local-awscli = pkgs.awscli;    # awscli.override { python = python37; };
 in
--- kafka.nix
+++ kafka.nix
@@ -1,15 +1,21 @@
 let
-  pkgs = import (builtins.fetchTarball {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-unstable-2020-05-08";
-    # Commit hash for nixos-unstable as of 2020-05-08 - get from head (git log)
-    url = https://github.com/nixos/nixpkgs/archive/d78ba41a5604c8e06d40756a2436e52169354d36.tar.gz;
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0bk81ddx1iq8i0nhg1i44kllihphyzhbc416zgylli0ycphknrkv";}) {
-      overlays = [ (self: super: {
-        jdk = use-jdk;
-        jre = use-jdk;
-      }) ];
+  pkgs =
+    import (
+      builtins.fetchTarball {
+        # Descriptive name to make the store path easier to identify
+        name = "nixos-unstable-2020-05-08";
+        # Commit hash for nixos-unstable as of 2020-05-08 - get from head (git log)
+        url = https://github.com/nixos/nixpkgs/archive/d78ba41a5604c8e06d40756a2436e52169354d36.tar.gz;
+        # Hash obtained using `nix-prefetch-url --unpack <url>`
+        sha256 = "0bk81ddx1iq8i0nhg1i44kllihphyzhbc416zgylli0ycphknrkv";
+      }
+    ) {
+      overlays = [ (
+        self: super: {
+          jdk = use-jdk;
+          jre = use-jdk;
+        }
+      ) ];
     };
 
   use-jdk = pkgs.jdk11;   # callPackage jdk/shared-jdk.nix { inherit jdk-name; inherit jdk-sha; };
