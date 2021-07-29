1b21d4d944c1fb92843e5a510d5c03a36654bf87
Andy Bryant <andybryant@gmail.com>
2021-07-29 11:50:04 +1000

    bump to terraform 1.0.x

--- terraform.nix
+++ terraform.nix
@@ -3,17 +3,17 @@ let
     import (
       builtins.fetchTarball {
         # Descriptive name to make the store path easier to identify
-        name = "nixos-unstable-2021-01-27";
-        # Commit hash for nixos-unstable as of 2021-01-27 - get from head (git log)
-        url = https://github.com/nixos/nixpkgs/archive/891f607d5301d6730cb1f9dcf3618bcb1ab7f10e.tar.gz;
+        name = "nixos-unstable-2021-07-27";
+        # Commit hash for nixos-unstable as of 2021-07-27 - get from head (git log)
+        url = https://github.com/nixos/nixpkgs/archive/dd14e5d78e90a2ccd6007e569820de9b4861a6c2.tar.gz;
         # Hash obtained using `nix-prefetch-url --unpack <url>`
-        sha256 = "1cr39f0sbr0h5d83dv1q34mcpwnkwwbdk5fqlyqp2mnxghzwssng";
+        sha256 = "1zmhwx1qqgl1wrrb9mjkck508887rldrnragvximhd7jrh1ya3fb";
       }
     ) {
     };
 
   select_plugins = p: [ p.google p.google-beta p.null ];
-  app_terraform = { full = pkgs.terraform_0_14.withPlugins select_plugins; };
+  app_terraform = { full = pkgs.terraform.withPlugins select_plugins; };
   app_terragrunt = pkgs.terragrunt;
 in
   pkgs.stdenv.mkDerivation rec {
@@ -21,7 +21,7 @@ in
 
     buildInputs = [
       #figlet
-      pkgs.terraform_0_14
+      pkgs.terraform
       pkgs.terragrunt
     ];
 
