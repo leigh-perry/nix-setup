e1f8d081d6c8d3df545d84f07e67110570464c44
Andy Bryant <andybryant@gmail.com>
2022-06-23 21:39:51 +1000

    fix date

--- terraform.nix
+++ terraform.nix
@@ -3,8 +3,8 @@ let
     import (
       builtins.fetchTarball {
         # Descriptive name to make the store path easier to identify
-        name = "nixos-unstable-2021-07-27";
-        # Commit hash for nixos-unstable as of 2021-07-27 - get from head (git log)
+        name = "nixos-unstable-2022-06-23";
+        # Commit hash for nixos-unstable as of 2022-06-23 - get from head (git log)
         url = https://github.com/nixos/nixpkgs/archive/e1e08fe28bf0588a41cd556eac40b98d2793da99.tar.gz;
         # Hash obtained using `nix-prefetch-url --unpack <url>`
         sha256 = "0abzal2gwzin9g7l4k2kll0hpjs3igacqq55k485fa4gb54s3f01";
